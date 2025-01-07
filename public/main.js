ec3cb0fcaa070037bd54d7212f85a896997148e3
Saltspeddy <walkercoco30@gmail.com>
2025-01-07 10:26:15 +0200

    fixed the js function changeColor, edited the README.md file and other small css changes

--- public/main.js
+++ public/main.js
@@ -1,15 +1,21 @@
 let contor = 1;
 
 function addCards(){
-    
     const b = document.querySelectorAll("#container");
     for( let i = 0 ; i < b.length ; i++){
         b[i].remove();
     }
 
     let height = parseInt( document.querySelector("#height").value);
-    console.log(height);
-    let length = parseInt( document.querySelector("#length").value); 
+    if( height > 40 ){
+        alert("the maximum input for height is 30")
+        return
+    } 
+    let length = parseInt( document.querySelector("#length").value);
+    if( length > 40 ){
+        alert("the maximum input for length is 30")
+        return
+    } 
     let parent = document.querySelector("#parent");  
     console.log(parent)     
     parent.innerHTML += '<main id="container" style="display:grid; height:100%; width:100%; grid-template-columns: repeat('+length+',1fr); grid-template-rows: repeat('+height+',1fr);"></main>'
@@ -17,21 +23,33 @@ function addCards(){
     for( let i = 1 ; i <= area ; i++){
         document.querySelector("#container").innerHTML += '<div class="blocks" ondragover="add(event)" onclick="addColor(event)"></div>';
         contor++;
-    }
-    
+    }              
 }
+
 let color
+let ok
+function switchMode(){
+    ok = 1
+}
+
 function changeColor(event){
     color = document.querySelector("#color").value;
-    color = "#" + color
+    if (color[0] != "#"){
+        color = "#" + color
+    }
 }
 
 function colorPicker(colorPicker){
     color = "#" + colorPicker
+    ok = -1
 }
 
 function addColor(event){
     let block = event.target;    
+    if (ok == 1){
+        changeColor()
+    }
+    console.log(ok)
     block.style.backgroundColor = color
     console.log(color)
 }
